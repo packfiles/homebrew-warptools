d34a71a81b1fb0b7b5bd827fc4699b9c7e17deb8
Packfiles Robot Army <svc-github-packages@packfiles.io>
2025-03-25 21:40:43 -0400

    Warp Vault v0.1.0

--- Casks/w/warp-vault.rb
+++ Casks/w/warp-vault.rb
@@ -1,9 +1,8 @@
 cask "warp-vault" do
-  version "1.0.0"
-  sha256 "587ff514d9789b2cd2d408c96218500d38cf9706593003d5f261608ab4caa69a"
+  version "v0.1.0"
+  sha256 "92ad375a8cb9fa419d251904928e205c0f8526226412df3c9a64cbb1b073a5b2"
 
-  url "https://github.com/packfiles/warp-vault/releases/download/v#{version}/Warp_Vault-v#{version}-macOS-universal.dmg", verified: "github.com/packfiles/warp-vault/releases"
-
+  url "https://github.com/packfiles/warp-vault/releases/download/v#{version}/Warp_Vault-v#{version}-macOS-universal.dmg", verified: "github.com/packfiles/warp-vault/releases/"
   name "Warp Vault"
   desc "End-to-end encrypted credential manager for Packfiles Warp"
   homepage "https://vault.packfiles.io/"
@@ -21,9 +20,5 @@ cask "warp-vault" do
 
   app "Warp Vault.app"
 
-  caveats <<-MSG
-  📦 Thanks for installing Warp Vault! 
-  
-  To get started, review our documentation at https://kb.packfiles.io.
-  MSG
+  caveats "📦 Thanks for installing Warp Vault! To get started, review our documentation at https://kb.packfiles.io."
 end
